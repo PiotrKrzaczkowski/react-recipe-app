c62cf97a7ef8e86e52d6a381d96422d830f93809
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-02-24 14:48:17 +0100

    get data functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState } from "react";
+import Axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App">pizdziocha zwachana</div>;
+  const [foodName, setFoodName] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const app_ID = "1c2ffc93";
+  const app_KEY = "ccb924fb4b07a1b3eddae7ac235ced46";
+  const URL = `https://api.edamam.com/search?q=${foodName}&app_id=${app_ID}&app_key=${app_KEY}`;
+
+  const getData = async () => {
+    const result = await Axios.get(URL);
+    setRecipes(result.data.hits);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      <h1>get data recipe</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="search food"
+          autoComplete="off"
+          value={foodName}
+          onChange={(e) => setFoodName(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
